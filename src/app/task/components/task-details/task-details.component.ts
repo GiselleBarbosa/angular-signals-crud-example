eed4416c57f4340fc749c331975648261fa97cb6
GiselleBarbosa <g88barbosa@gmail.com>
2023-07-18 21:11:19 -0300

    feat: addead taks service

--- src/app/task/components/task-details/task-details.component.ts
+++ src/app/task/components/task-details/task-details.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './task-details.component.html',
-  styleUrls: ['./task-details.component.scss']
+  styleUrls: ['./task-details.component.scss'],
+  providers: [TaskService],
 })
 export class TaskDetailsComponent {
+  public selecterUserId!: number;
 
+  public userService = inject(UserService);
+
+  public route = inject(ActivatedRoute);
+
+  public router = inject(Router);
+
+  public ngOnInit(): void {
+    this.selecterUserId = +this.route.snapshot.paramMap.get('id')!;
+
+    if (this.selecterUserId) {
+      this.userService.setSelectedUserId(this.selecterUserId);
+    } else {
+      this.router.navigateByUrl('/');
+    }
+  }
 }
--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap, tap } from 'rxjs';
+import { Task } from 'src/task/task';
+import { UserService } from '../user/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  public http = inject(HttpClient);
 
-  constructor() { }
+  public userService = inject(UserService);
+
+  public usersUrl = 'http://localhost:3000/users';
+
+  public tasksUrl = 'http://localhost:3000/tasks';
+
+  public userTasks = signal<Task[]>([]);
+
+  private userTasks$ = toObservable(this.userService.selectedUserId).pipe(
+    switchMap((userId) =>
+      this.http.get<Task[]>(`${this.tasksUrl}?userId=${userId}`).pipe(
+        tap((tasks) => {
+          this.userTasks.set(tasks);
+        }),
+      ),
+    ),
+  );
+  public readOnlyUserTasks = toSignal(this.userTasks$, {
+    initialValue: [] as Task[],
+  });
 }
